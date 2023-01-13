6861ef9eb117f0a9cd9b30138de480313dc462a1
locnguyen273 <77155886+locnguyen273@users.noreply.github.com>
2023-01-13 18:10:28 +0700

    [add] add FE client page home and config api product (#3)

---
+++ client/src/types/index.ts
@@ -0,0 +1,6 @@
+export interface CategoryType {
+  id: number;
+  img: string;
+  title: string;
+  cat: string;
+}
--- shopping-cart-api/models/Product.js
+++ shopping-cart-api/models/Product.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const ProductSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, unique: true },
-    desc: { type: String, required: true, },
     img: { type: String, required: true },
+    imgDetail: { type: Array, required: true },
     categories: { type: Array },
-    size: { type: String },
+    size: { type: Array },
     color: { type: String },
     price: { type: Number, required: true },
     inStock: { type: Boolean, default: true }
