ac2e74920182d348f67485042a3b0c794a02fd79
locnguyen273 <77155886+locnguyen273@users.noreply.github.com>
2023-01-18 15:11:53 +0700

    1 (#5)

--- client/src/components/Products/index.tsx
+++ client/src/components/Products/index.tsx
@@ -1,25 +1,61 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect } from "react"
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchListProduct } from "../../redux/actions/productAction";
 import { AppDispatch } from "../../redux/configStore";
 import Product from "../Product";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { Link } from "react-router-dom";
 
 const Products = () => {
   const dispatch: AppDispatch = useDispatch();
   useEffect(() => {
-    dispatch(fetchListProduct())
-  },[]);
+    dispatch(fetchListProduct());
+  }, []);
 
   return (
     <div>
-      <Product/>
-      <Product/>
-      <Product/>
-      <Product/>
-
+      <Box sx={{ flexGrow: 1 }} style={{ maxWidth: "85vw", margin: "auto" }}>
+        <Grid
+          container
+          spacing={2}
+          style={{ margin: "auto", marginBottom: "2rem", marginTop: "1rem" }}
+        >
+          <Grid item lg={2.4} md={3} sm={4} xs={12}>
+            <Link to="/cart" style={{textDecoration: "none"}}>
+              <Product />
+            </Link>
+          </Grid>
+          <Grid item lg={2.4} md={3} sm={4} xs={12}>
+            <Link to="/cart" style={{textDecoration: "none"}}>
+              <Product />
+            </Link>
+          </Grid>
+          <Grid item lg={2.4} md={3} sm={4} xs={12}>
+            <Link to="/cart" style={{textDecoration: "none"}}>
+              <Product />
+            </Link>
+          </Grid>
+          <Grid item lg={2.4} md={3} sm={4} xs={12}>
+            <Link to="/cart" style={{textDecoration: "none"}}>
+              <Product />
+            </Link>
+          </Grid>
+          <Grid item lg={2.4} md={3} sm={4} xs={12}>
+            <Link to="/cart" style={{textDecoration: "none"}}>
+              <Product />
+            </Link>
+          </Grid>
+          <Grid item lg={2.4} md={3} sm={4} xs={12}>
+            <Link to="/cart" style={{textDecoration: "none"}}>
+              <Product />
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
--- client/src/components/Slider/index.tsx
+++ client/src/components/Slider/index.tsx
@@ -1,111 +1,91 @@
-import { useState } from "react";
-import styled from "styled-components";
+import { useEffect, useState } from "react";
 import { sliderItems } from "../../assets/data";
-import { mobile } from "../../hooks/responsive";
+import Slider from "react-slick";
 import ArrowBackIosNewOutlinedIcon from "@mui/icons-material/ArrowBackIosNewOutlined";
 import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
+import "./style.scss";
 
-const Container = styled.div`
-  width: 100%;
-  height: 80vh;
-  margin: 3rem 0 1rem;
-  display: flex;
-  position: relative;
-  overflow: hidden;
-  ${mobile({
-    display: "none",
-  })}
-`;
-const Arrow = styled.div<any>`
-  width: 50px;
-  height: 50px;
-  background-color: #fff7f7;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: ${(props) => props.direction === "left" && "10px"};
-  right: ${(props) => props.direction === "right" && "10px"};
-  margin: auto;
-  cursor: pointer;
-  opacity: 0.5;
-  z-index: 2;
-`;
-const Wrapper = styled.div<any>`
-  height: 100%;
-  display: flex;
-  transition: all 0.5s ease-in-out;
-  transform: translateX(${(props) => props.sliderIndex * -100}vw);
-`;
-const Slide = styled.div<any>`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  background-color: #${(props) => props.bg};
-`;
-const ImgContainer = styled.div`
-  height: 100%;
-  flex: 1;
-`;
-const Image = styled.img`
-  height: 80%;
-`;
-const InfoContainer = styled.div`
-  flex: 1;
-  padding: 50px;
-`;
-const Title = styled.h1`
-  font-size: 70px;
-`;
-const Desc = styled.p`
-  margin: 50px 0px;
-  font-size: 20px;
-  font-weight: 500;
-  letter-spacing: 3px;
-`;
-const Button = styled.button`
-  padding: 10px;
-  font-size: 20px;
-  background-color: transparent;
-  cursor: pointer;
-`;
+export interface ISlider {
+  id: number;
+  img: string;
+  imgMobile: string;
+  title: string;
+}
+
+function SampleNextArrow(props: any) {
+  const { style, onClick } = props;
+  return (
+    <div
+      style={{
+        ...style,
+        position: "absolute",
+        top: "50%",
+        right: "2%"
+      }}
+      onClick={onClick}
+    >
+      <ArrowForwardIosOutlinedIcon />
+    </div>
+  );
+}
+
+function SamplePrevArrow(props: any) {
+  const { style, onClick } = props;
+  return (
+    <div
+      style={{
+        ...style,
+        position: "absolute",
+        top: "50%",
+        left: "2%"
+      }}
+      onClick={onClick}
+    >
+      <ArrowBackIosNewOutlinedIcon />
+    </div>
+  );
+}
 
 const Sliders = () => {
-  const [slideIndex, setSlideIndex] = useState(0);
-  const handleClick = (direction: any) => {
-    if(direction === "left") {
-      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
-    } else {
-      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const updateSize = () => {
+      if(window.innerWidth <= 425) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
     }
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, [setIsMobile]);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
   };
+
   return (
-    <Container>
-      <Arrow direction="left" onClick={() => handleClick("left")}>
-        <ArrowBackIosNewOutlinedIcon />
-      </Arrow>
-      <Wrapper slideIndex={slideIndex}>
-        {sliderItems.map((item) => (
-          <Slide bg={item.bg} key={item.id}>
-            <ImgContainer>
-              <Image src={item.img} />
-            </ImgContainer>
-            <InfoContainer>
-              <Title>{item.title}</Title>
-              <Desc>{item.desc}</Desc>
-              <Button>SHOW NOW</Button>
-            </InfoContainer>
-          </Slide>
-        ))}
-      </Wrapper>
-      <Arrow direction="right" onClick={() => handleClick("right")}>
-        <ArrowForwardIosOutlinedIcon />
-      </Arrow>
-    </Container>
+    <Slider {...settings} className="slider">
+      {sliderItems.length > 0 &&
+        sliderItems.map((item: ISlider) => {
+          return (
+            <div key={item.id}>
+              <img
+                className="slider__image"
+                src={isMobile ? item.imgMobile : item.img}
+                alt={item.title}
+              />
+            </div>
+          );
+        })}
+    </Slider>
   );
 };
 
