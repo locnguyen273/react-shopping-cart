eb64fa84f67f82acbf6ef19bfcdf60ea1c2e0055
Loc Nguyen <tanlocit20062000@gmail.com>
2023-01-19 18:16:35 +0700

    [add] custom header

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,159 +1,111 @@
-import React, { useState } from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
 import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
+import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
-
-const pages = ["Trang Chủ", "Sản Phẩm", "Tin Tức", "Hệ Thống Cửa Hàng"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
+import { Link, useLocation } from "react-router-dom";
+import "./style.scss";
+import { menuPages, loggedInMenu, noLoggedInMenu } from "../../assets/data";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
 const Header = () => {
-  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
-  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+  const logged = false;
+  const location = useLocation();
+  const [active, setActive] = useState<string>("");
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
 
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
+  useEffect(() => {
+    setActive(location.pathname);
+  }, []);
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
   };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
   return (
-    <AppBar position="static" style={{ background: '#2E3B55' }}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h4"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            Bamboo Shop
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            Bamboo Shop
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "white", display: "block" }}
+    <div className="header">
+      <Link to="/" className="header__left">
+        <Typography variant="h2">Bamboo Shop</Typography>
+      </Link>
+      <div className="header__center">
+        {menuPages.length > 0 &&
+          menuPages.map((item, index) => {
+            return (
+              <Link
+                to={item.pathName}
+                key={index}
+                onClick={() => setActive(item.pathName)}
+                className={`header__center--item ${
+                  active === item.pathName && "active"
+                }`}
               >
-                {page}
-              </Button>
-            ))}
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+                {item.label}
+              </Link>
+            );
+          })}
+      </div>
+      <div className="header__right">
+        <Button
+          id="basic-button"
+          aria-controls={open ? "basic-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+        >
+          <AccountCircleRoundedIcon style={{ fontSize: "2.5rem" }} />
+        </Button>
+        <Link to="/cart" className="header__right__cart">
+          <ShoppingCartOutlinedIcon />
+          <div className="header__right__cart--info">
+            <p>Giỏ hàng</p>
+            <p>( 0 Sản phẩm )</p>
+          </div>
+        </Link>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          {logged
+            ? loggedInMenu.length > 0 &&
+              loggedInMenu.map((item, index) => {
+                return (
+                  <MenuItem key={index}>
+                    <Link
+                      to={item.pathName}
+                      className="header__right__menu--item"
+                    >
+                      {item.label}
+                    </Link>
+                  </MenuItem>
+                );
+              })
+            : noLoggedInMenu.length > 0 &&
+              noLoggedInMenu.map((item, index) => {
+                return (
+                  <MenuItem key={index}>
+                    <Link
+                      to={item.pathName}
+                      className="header__right__menu--item"
+                    >
+                      {item.label}
+                    </Link>
+                  </MenuItem>
+                );
+              })}
+        </Menu>
+      </div>
+    </div>
   );
 };
 
